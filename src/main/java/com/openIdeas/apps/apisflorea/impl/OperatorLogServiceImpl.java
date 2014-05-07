b90aadbeee4fa8c91bf29a4626e49d4812297d02
evanmu <13010158@SN-CNHQ-3113T.sn.suning.ad>
2014-05-07 17:41:02 +0800

    短信发送日志方法

--- src/main/java/com/openIdeas/apps/apisflorea/impl/OperatorLogServiceImpl.java
+++ src/main/java/com/openIdeas/apps/apisflorea/impl/OperatorLogServiceImpl.java
@@ -1,12 +1,26 @@
 package com.openIdeas.apps.apisflorea.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.openIdeas.apps.apisflorea.dao.SmsOpLogDao;
+import com.openIdeas.apps.apisflorea.entity.SmsOpLog;
 import com.openIdeas.apps.apisflorea.intf.OperatorLogServiceIntf;
+import com.openIdeas.apps.apisflorea.result.CollectionResult;
 
 public class OperatorLogServiceImpl implements OperatorLogServiceIntf {
-	@Autowired
-	private SmsOpLogDao smsOpLogDao;
+    @Autowired
+    private SmsOpLogDao smsOpLogDao;
+
+    /*
+     * (non-Javadoc)
+     * @see com.openIdeas.apps.apisflorea.intf.OperatorLogServiceIntf#getOplog()
+     */
+    @Override
+    public CollectionResult<List<SmsOpLog>> getOplog() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
--- src/main/java/com/openIdeas/apps/apisflorea/intf/OperatorLogServiceIntf.java
+++ src/main/java/com/openIdeas/apps/apisflorea/intf/OperatorLogServiceIntf.java
@@ -1,7 +1,11 @@
 package com.openIdeas.apps.apisflorea.intf;
 
+import java.util.List;
+
+import com.openIdeas.apps.apisflorea.entity.SmsOpLog;
+import com.openIdeas.apps.apisflorea.result.CollectionResult;
 
 public interface OperatorLogServiceIntf {
-    
-    
+
+    CollectionResult<List<SmsOpLog>> getOplog();
 }
